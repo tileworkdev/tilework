cbc30113abdcaade9f675e2b4406ab6949c10a45
Alexandros Nikolopoulos <a.nikolopoulos@tilework.dev>
2024-05-02 16:37:38 +0000

    custom ports

--- loadbalancing.tile/LoadBalancerService.cs
+++ loadbalancing.tile/LoadBalancerService.cs
@@ -46,21 +46,21 @@ public class LoadBalancerService
             Id = Guid.Parse("e8d5f029-4996-4bc1-86ec-5a92f0194b49"),
             Name = "LB1",
             Type = LoadBalancerType.NETWORK,
-            Port = 8080,
+            Port = 6507,
             Group = targetGroups[0],
         },
         new LoadBalancer {
             Id = Guid.Parse("a550a4bd-2330-45b6-800f-345c1ffb5ff7"),
             Name = "LB2",
             Type = LoadBalancerType.APPLICATION,
-            Port = 443,
+            Port = 6508,
             Group = targetGroups[1],
         },
         new LoadBalancer {
             Id = Guid.Parse("a98a013d-31de-4a43-b990-c49658d3ed38"),
             Name = "LB3",
             Type = LoadBalancerType.NETWORK,
-            Port = 53,
+            Port = 6509,
             Group = targetGroups[2]
         }
     };
