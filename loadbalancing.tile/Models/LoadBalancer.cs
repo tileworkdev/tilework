648cd83461c03880ecd785f3885dfa584e7e47a1
Alexandros Nikolopoulos <a.nikolopoulos@tilework.dev>
2024-05-04 09:18:13 +0000

    Simplified configuration

--- loadbalancing.tile/Models/LoadBalancer.cs
+++ loadbalancing.tile/Models/LoadBalancer.cs
@@ -7,8 +7,6 @@ public class LoadBalancer
     public Guid Id { get; set; }
     public string Name { get; set; }
     public LoadBalancerType Type { get; set; }
-    public NlbProtocol? NlbProtocol { get; set; }
-    public AlbProtocol? AlbProtocol { get; set; }
     public int Port { get; set; }
     public TargetGroup Group { get; set; }
     public bool Enabled { get; set; }
