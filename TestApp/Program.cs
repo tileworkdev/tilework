2734ae4b7ced7ead9380da12ca3c67c510aeb8ca
Alexandros Nikolopoulos <a.nikolopoulos@tilework.dev>
2024-12-26 14:15:09 +0000

    Removed testapp

--- TestApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Tilework.LoadBalancing.Haproxy;
-using Tilework.LoadBalancing.Services;
-
-
-var cfg = new HAProxyConfigurator();
-
-var lb = new LoadBalancerService(cfg);
-lb.ApplyConfiguration();
