c99eddbcf1764600e731b8bbf5cceda37405b318
Alexandros Nikolopoulos <alxnik@gmail.com>
2025-08-11 13:24:08 +0300

    Move DbContext helpers to core with exception processor (#11)

---
+++ tilework.core/DbContextMigrationsHelper.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Tilework.Core;
+
+public static class DbContextMigrationsHelper
+{
+    public static void RunDbMigrations(this IApplicationBuilder app, IServiceCollection services)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        var logger = serviceProvider.GetRequiredService<ILogger<IApplicationBuilder>>();
+
+        var dbContextTypes = services
+            .Where(service => service.ServiceType.IsSubclassOf(typeof(DbContext)))
+            .Select(service => service.ServiceType);
+
+        foreach (var dbContextType in dbContextTypes)
+        {
+            var dbContext = (DbContext)serviceProvider.GetRequiredService(dbContextType);
+            logger.LogInformation($"Running migrations for context: {dbContextType}");
+            dbContext.Database.Migrate();
+        }
+    }
+}
--- tilework.core/DbContextOptionsHelper.cs
+++ tilework.core/DbContextOptionsHelper.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using EntityFramework.Exceptions.Sqlite;
+
+namespace Tilework.Core;
 
 public static class DbContextOptionsHelper
 {
     public static Action<DbContextOptionsBuilder> Configure(string connectionString)
     {
-        return options => options.UseSqlite(connectionString);
+        return options => options
+            .UseSqlite(connectionString)
+            .UseExceptionProcessor();
     }
 }
