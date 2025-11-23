621df2275d8eccc990d7ddcd6f5440101db2680c
Alexandros Nikolopoulos <a.nikolopoulos@tilework.dev>
2025-11-23 11:22:32 +0000

    Refactoring statistics

--- tilework.core/Providers/LoadBalancingProviders/HAProxy/Mappers/HAProxyMonitoringProfile.cs
+++ tilework.core/Providers/LoadBalancingProviders/HAProxy/Mappers/HAProxyMonitoringProfile.cs
@@ -7,9 +7,9 @@ public class HAProxyMonitoringProfile : Profile
 {
     public HAProxyMonitoringProfile()
     {
-        CreateMap<(HAProxyInfo, HAProxyStatisticsRow), LoadBalancingStatistics>()
-            .ForMember(dest => dest.Uptime, opt => opt.MapFrom(src => TimeSpan.FromSeconds(Int32.Parse(src.Item1.Uptime_sec))))
-            .ForMember(dest => dest.TotalSessions, opt => opt.MapFrom(src => src.Item2.stot));
+        CreateMap<(HAProxyInfo, HAProxyStatisticsRow), LoadBalancingMonitoringData>();
+            // .ForMember(dest => dest.Uptime, opt => opt.MapFrom(src => TimeSpan.FromSeconds(Int32.Parse(src.Item1.Uptime_sec))))
+            // .ForMember(dest => dest.TotalSessions, opt => opt.MapFrom(src => src.Item2.stot));
             // .ForMember(dest => dest.CurrentSessions, opt => opt.MapFrom(src => src.scur))
             // .ForPath(dest => dest.CurrentQueue, opt => opt.MapFrom(src => src.qcur));
     }
