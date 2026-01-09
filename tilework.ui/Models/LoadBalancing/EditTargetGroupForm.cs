ea94146d4494e694c6c44eacda1910319de6408a
Alexandros Nikolopoulos <a.nikolopoulos@tilework.dev>
2026-01-09 15:50:53 +0000

    GenericEditView can now handle multiple forms

--- tilework.ui/Models/LoadBalancing/EditTargetGroupForm.cs
+++ tilework.ui/Models/LoadBalancing/EditTargetGroupForm.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 namespace Tilework.Ui.Models;
 
 public class EditTargetGroupForm : BaseForm
 {
+    [Required]
     public string Name { get; set; }
 }
--- tilework.ui/Models/LoadBalancing/NewTargetGroupForm.cs
+++ tilework.ui/Models/LoadBalancing/NewTargetGroupForm.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Tilework.LoadBalancing.Enums;
 
 namespace Tilework.Ui.Models;
 
 public class NewTargetGroupForm : BaseForm
 {
+    [Required]
     public string Name { get; set; }
     public TargetGroupProtocol Protocol { get; set; } 
 }
